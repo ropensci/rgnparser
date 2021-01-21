38d2aeff2b5b3990346a98a00f0757f90998e4fe
Scott Chamberlain <sckott@fastmail.com>
2021-01-21 09:56:37 -0800

    fix #9 gn_debug now defunct, funtionality removed from gnparser in v1

--- R/gn_debug.R
+++ R/gn_debug.R
@@ -1,37 +1,9 @@
 #' gn_debug
 #'
-#' debug parsing: get complete syntax tree for names
+#' DEFUNCT
 #'
 #' @export
-#' @param x (character) vector of scientific names
-#' @param threads (integer/numeric) number of threads to run. CPU's
-#' threads number is the default. default: 4
-#' @return cats output to screen
-#' @examples
-#' trys <- function(x) try(x, silent=TRUE)
-#' z <- c("Quadrella steyermarkii (Standl.) Iltis &amp; Cornejo",
-#'   "Parus major Linnaeus, 1788", "Helianthus annuus var. texanus")
-#' if (interactive()) {
-#' trys(gn_debug(z[1]))
-#' }
-gn_debug <- function(x, threads = 4) {
-  gnparser_exists()
-
-  assert(x, "character")
-  assert(threads, c("integer", "numeric"))
-
-  args <- character(0)
-  args <- c(args, "-f", "debug")
-  if (!is.null(threads)) args <- c(args, "-j", threads)
-
-  z <- gnparser_cmd(c(args, x), error = FALSE)
-  err_chk(z)
-  parsed <- rawToChar(z$stdout)
-  return(structure(parsed, class="gnparse_debug"))
-}
-
-#' @export
-print.gnparse_debug <- function(x, ...) {
-  cat("<rgnparser debug>", sep = "\n")
-  cat(x)
+#' @param ... ignored
+gn_debug <- function(...) {
+  .Defunct(msg="gn_debug is defunct, functionality was removed from gnparser")
 }
--- tests/testthat/test-gn_debug.R
+++
@@ -1,8 +0,0 @@
-skip_on_cran()
-
-test_that("gn_debug", {
-  z <- gn_debug("Parus major Linnaeus, 1788")
-
-  expect_is(z, "gnparse_debug")
-  expect_is(unclass(z), "character")
-})
