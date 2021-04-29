445098008471ddce0f2eb1c2dca5e1edcdc401bc
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2021-04-29 08:36:12 -0300

    test the version installed by install_gnparser()

---
+++ tests/testthat/test-install_gnparser.R
@@ -0,0 +1,10 @@
+skip_on_cran()
+
+test_that("install_gnparser skip the nightly version", {
+    install_gnparser()
+    json <- jsonlite::fromJSON('https://api.github.com/repos/gnames/gnparser/releases')
+    nightly <- json$tag_name[1]
+    latest <- json$tag_name[2]
+    expect_equal(nightly, "nightly")
+    expect_equal(gn_version()$version, latest)
+})
