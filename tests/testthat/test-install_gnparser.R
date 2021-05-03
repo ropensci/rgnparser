96a32b0151e523445fffc88cf8d160edfc30aec9
Scott Chamberlain <sckott@fastmail.com>
2021-05-03 15:31:04 -0700

    in install_gnparser test, skip if curl not installed

--- tests/testthat/test-install_gnparser.R
+++ tests/testthat/test-install_gnparser.R
@@ -1,10 +1,12 @@
 skip_on_cran()
 
 test_that("install_gnparser skip the nightly version", {
-    install_gnparser()
-    json <- jsonlite::fromJSON('https://api.github.com/repos/gnames/gnparser/releases')
-    nightly <- json$tag_name[1]
-    latest <- json$tag_name[2]
-    expect_equal(nightly, "nightly")
-    expect_equal(gn_version()$version, latest)
+  skip_if_not_installed("curl")
+
+  install_gnparser()
+  json <- jsonlite::fromJSON('https://api.github.com/repos/gnames/gnparser/releases')
+  nightly <- json$tag_name[1]
+  latest <- json$tag_name[2]
+  expect_equal(nightly, "nightly")
+  expect_equal(gn_version()$version, latest)
 })
