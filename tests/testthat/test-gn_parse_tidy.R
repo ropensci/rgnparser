4e1b3bc915938a87aac9c6c6c9f117609f4cd95d
Joel Nitta <joelnitta@gmail.com>
2023-09-15 17:18:27 +0900

    Skip tests requiring gnparser on CI

--- tests/testthat/test-gn_parse_tidy.R
+++ tests/testthat/test-gn_parse_tidy.R
@@ -1,4 +1,5 @@
 skip_on_cran()
+skip_on_ci()
 
 test_that("gn_parse_tidy", {
   x <- c("Quadrella steyermarkii (Standl.) Iltis & Cornejo",
--- tests/testthat/test-gn_version.R
+++ tests/testthat/test-gn_version.R
@@ -1,4 +1,5 @@
 skip_on_cran()
+skip_on_ci()
 
 test_that("gn_version", {
   expect_is(gn_version(), "list")
